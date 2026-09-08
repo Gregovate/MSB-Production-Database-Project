e8d902a6d7fe4097d7f1fbe5e85d9d514eb2c48b
Gregovate <123017250+Gregovate@users.noreply.github.com>
2026-09-07 23:20:24 -0500

    Remove incorrect resource visibility regression assertion

--- Setup/Application/test_setup_live_review_fixes_contract.py
+++ Setup/Application/test_setup_live_review_fixes_contract.py
@@ -47,19 +47,3 @@ def test_live_review_restores_task_and_stage_search() -> None:
     assert "renderLibraryWithLiveSearch" in js
     for field in ("task_name", "stage_key", "stage_name", "scene_name", "task_action_type"):
         assert field in js
-
-
-def test_live_review_keeps_resource_manager_controls_visible_for_managers() -> None:
-    js = (APP_DIR / "setup_live_review_fixes.js").read_text(encoding="utf-8")
-    resource_js = (APP_DIR / "setup_resource_review.js").read_text(encoding="utf-8")
-
-    assert "syncAuthorizedResourceEditor" in js
-    assert "setup-resource-manager" in js
-    assert "can_manage_setup" in js
-    assert "applyAccessWithLiveFixes" in js
-    assert "renderSetupTaskResourcesWithManagerAccess" in js
-
-    assert "Add / Update Resource" in resource_js
-    assert "Create Resource" in resource_js
-    assert "api/setup/resources" in resource_js
-    assert "api/setup/tasks/${task.setup_task_id}/resources/${resourceId}" in resource_js
