453d1efc28bfb243b08a4b491d0e0172fa208c7f
Gregovate <123017250+Gregovate@users.noreply.github.com>
2026-09-08 07:58:08 -0500

    Normalize Setup scenes before every library render

--- Setup/Application/setup_live_review_fixes.js
+++ Setup/Application/setup_live_review_fixes.js
@@ -239,6 +239,7 @@
 
   const priorRenderLibrary = renderLibrary;
   renderLibrary = function renderLibraryWithLiveSearch() {
+    normalizeCurrentSetupOrganization();
     priorRenderLibrary();
     applyLibrarySearch();
   };
