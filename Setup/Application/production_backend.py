4962ff4afda0a2540d5390980d927a8a2a35b3d4
Gregovate <123017250+Gregovate@users.noreply.github.com>
2026-09-08 16:26:19 -0500

    Serve Assigned reconciliation browser asset

--- Setup/Application/production_backend.py
+++ Setup/Application/production_backend.py
@@ -41,6 +41,7 @@ PRODUCTION_ASSETS = frozenset(
         "setup_live_review_fixes.js",
         "setup_training_ux.css",
         "setup_training_ux.js",
+        "setup_assigned_review.js",
     }
 )
 
