87d1a2b62811374c41b32c0dd2061662629550b4
Gregovate <123017250+Gregovate@users.noreply.github.com>
2026-09-11 09:37:00 -0500

    Version #152 candidate as V0.3.10 resource catalog

--- Setup/Application/production_backend.py
+++ Setup/Application/production_backend.py
@@ -21,7 +21,7 @@ from setup_material_api import setup_material_api
 from setup_prerequisite_order_api import setup_prerequisite_order_api
 from setup_material_resolution import install_setup_material_resolution
 
-PRODUCTION_VERSION = "V0.3.9-predecessor-drag"
+PRODUCTION_VERSION = "V0.3.10-resource-catalog"
 PRODUCTION_ASSETS = frozenset(
     {
         "setup.css",
