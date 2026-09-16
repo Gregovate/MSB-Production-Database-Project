087272b9692e167e415ca83681dd6d44a2d768fb
Gregovate <123017250+Gregovate@users.noreply.github.com>
2026-09-15 21:45:21 -0500

    Setup #198 serve expected source editor asset

--- Setup/Application/production_backend.py
+++ Setup/Application/production_backend.py
@@ -79,6 +79,7 @@ PRODUCTION_ASSETS = frozenset(
         "setup_extra_materials.css",
         "setup_extra_materials.js",
         "setup_task_extra_materials.js",
+        "setup_task_extra_material_sources.js",
         "setup_uom_catalog.js",
         "setup_planning_summary.js",
     }
