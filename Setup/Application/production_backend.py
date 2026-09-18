f1af1f44ed7917bbe613d6d8994809e423e59704
Gregovate <123017250+Gregovate@users.noreply.github.com>
2026-09-18 11:43:17 -0500

    #205 set Setup V0.3.14 scheduling-board version

--- Setup/Application/production_backend.py
+++ Setup/Application/production_backend.py
@@ -30,7 +30,7 @@ from setup_display_ownership import install_setup_display_ownership
 from setup_assignment_layer import install_setup_assignment_layer
 from setup_kit_box_catalog_fix import install_setup_kit_box_catalog_fix
 
-PRODUCTION_VERSION = "V0.3.13-assignment-layer"
+PRODUCTION_VERSION = "V0.3.14-scheduling-board"
 PRODUCTION_ASSETS = frozenset(
     {
         "setup.css",
