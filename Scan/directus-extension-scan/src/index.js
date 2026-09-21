8098ebca303995e452d9ef420204b1b9a7483890
Gregovate <123017250+Gregovate@users.noreply.github.com>
2026-09-20 22:23:01 -0500

    #219 preserve rendered browser script with raw template

--- Scan/directus-extension-scan/src/index.js
+++ Scan/directus-extension-scan/src/index.js
@@ -440,7 +440,7 @@ export default {
         ].join('; ')
       );
 
-      res.send(`
+      res.send(String.raw`
         <!doctype html>
         <html>
         <head>
@@ -1325,7 +1325,7 @@ export default {
               exportFile(
                 'msb-scan-gps-field-test-' + session.session_id + '.csv',
                 'text/csv',
-                rows.join('\\n')
+                rows.join('\n')
               );
             }
 
