e89f965f069cae2f2ff640cce4720a3a3752929b
Gregovate <123017250+Gregovate@users.noreply.github.com>
2026-09-23 16:53:24 -0500

    #222 pin corrected performance trace runner identity

--- Setup/Application/test_setup_222_performance_trace_production_deploy_contract.py
+++ Setup/Application/test_setup_222_performance_trace_production_deploy_contract.py
@@ -14,7 +14,7 @@ def test_222_wrapper_is_source_only_and_pinned() -> None:
     assert "$ExpectedBranch = 'main'" in wrapper
     assert "1b08bdd26156b67ba89ea484fdc035b0b09ffc28" in wrapper
     assert "64835504962247d7a09c1146e5e77d8e19948559" in wrapper
-    assert "63715597c0d64d5ec5da4db16284e2e65115fe39" in wrapper
+    assert "59a01d1cbd87f6044a7d2b3a3badcf728b4e38a2" in wrapper
     assert "merge-base --is-ancestor" in wrapper
     assert "hash-object $ServerScript" in wrapper
     assert "scp -r $localBundle" in wrapper
